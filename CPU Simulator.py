b0f2da0d33c6a44ced63885a7e81c04ea701375a
Faruk Kaygisiz <kaygisizfaruk68@gmail.com>
2025-01-24 16:04:28 +0000

    Add binary instruction reader to CPU Simulator for opcode processing

--- CPU Simulator.py
+++ CPU Simulator.py
@@ -69,6 +69,39 @@ class UltraSuperCalculator:
         self.temp_history_index -= 1
         last_value = "The last value is: " + int(self.history_registers[self.temp_history_index], 2)
     
-
+    def binary_reader(self, instruction):
+        if len(instruction) != 32:
+            self.update_display("Error: Instruction length must be 32 bits")
+            return
+        opcode = instruction[0:6]
+        source_one = instruction[6:11]
+        source_two = instruction[11:16]
+        store = instruction[16:26]
+        function_code = instruction[26:]
+        if opcode == "000001":
+            self.store_value_to_register(store)
+            return
+        elif opcode == "100001":
+            self.get_last_calculation()
+            return
+        elif opcode != "000000":
+            self.update_display("Error: Invalid opcode")
+            return
+        result = 0
+        if function_code == "100000":
+            result = self.add(source_one, source_two)
+        elif function_code == "100010":
+            result = self.subtract(source_one, source_two)
+        elif function_code == "011000":
+            result = self.multiply(source_one, source_two)
+        elif function_code == "011010":
+            result = self.divide(source_one, source_two)
+        else:
+            self.update_display("Error: Invalid function code")
+            return
+        self.store_to_history_register(result)
+        self.update_display(f"Result: {result}")
 
 your_calculator = UltraSuperCalculator("Ultra Super Calculator")
+your_calculator.binary_reader("00000100000000000000000101000000")
+your_calculator.binary_reader("00000100000000000000001010000000")
